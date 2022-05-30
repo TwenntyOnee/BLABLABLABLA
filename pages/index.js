3bc326b699953e5cff4e2593b5b2aafa57225ce0
TwenntyOnee <kristian.ulrych@gmail.com>
2022-05-30 16:21:33 +0200

    sdsdsd

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">THIS SHIT</a>
+          Welcome to <a href="https://nextjs.org">THIS AWESOME WEBSITE</a>
         </h1>
 
         <p className={styles.description}>
